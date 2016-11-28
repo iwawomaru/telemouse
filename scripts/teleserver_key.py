a274a7266c372fb8924916191aebb43e3e79c451
iwawomaru <mosawa@ailab.ics.keio.ac.jp>
2016-11-28 19:30:33 +0900

    add disconfort message to teleserver_key.py

--- scripts/teleserver_key.py
+++ scripts/teleserver_key.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import Int16
 import pygame
 
 pygame.init()
@@ -10,9 +11,13 @@ pygame.display.set_mode((10,10), 0, 32)
 screen = pygame.display.get_surface()
 
 rospy.init_node("teleserver_key")
-pub = rospy.Publisher("direction", Int16MultiArray, queue_size=1)
+pub_dic = rospy.Publisher("direction", Int16MultiArray, queue_size=1)
+pub_dconf = rospy.Publisher("disconfort", Int16, queue_size=1)
+
 rate = rospy.Rate(10)
 array = Int16MultiArray()
+disconf = Int16()
+
 pos = [1, 1]
 while not rospy.is_shutdown():
     pygame.display.update()
@@ -23,6 +28,7 @@ while not rospy.is_shutdown():
     # print "pressed_keys:", pressed_keys
     
     f_keydown = False
+    disconf.data = 0
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -36,11 +42,17 @@ while not rospy.is_shutdown():
                 pos[1] -= 1
             if event.key == K_DOWN:
                 pos[1] += 1
+            
+            if event.key == K_SPACE:
+                disconf.data = 1
+
         if event.type == KEYUP:
             pos = [1, 1]
+            
 
     array.data[pos[1]*3+pos[0]] = 1
 
-    pub.publish(array)
+    pub_dis.publish(array)
+    pub_dconf.publish(disconf)
     rate.sleep()
     
