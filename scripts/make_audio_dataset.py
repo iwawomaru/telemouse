1f452fdce247dca493b8c7dac20766a8fca812d3
iwawomaru <mosawa@ailab.ics.keio.ac.jp>
2016-11-29 17:56:03 +0900

    successful disconform learning

--- scripts/make_audio_dataset.py
+++ scripts/make_audio_dataset.py
@@ -6,16 +6,28 @@ from audio_common_msgs.msg import AudioData
 import os, sys
 import numpy as np
 
-def callback(audio):
+class MakeAudioDataset(object):
+    def __init__(self):
+        self.disconf = -1
+        self.dataset = []
 
-    int_array = [ord(n) for n in audio.data]
-    print int_array
+        rospy.init_node("audio_trainer")
+        audio_sub = rospy.Subscriber("audio", AudioData, self.audio_callback)
+        disconf_sub = rospy.Subscriber("disconfort", Int16, self.disconf_callback)
 
-    # print np.array(audio.data, dtype=np.uint8)
-    """
-    for d in np.array(audio.data, dtype=np.uint8):
-        print d
-    """
-rospy.init_node("audio_trainer")
-sub = rospy.Subscriber("audio", AudioData, callback)
+    def __del__(self):
+        dataset = np.array(self.dataset)
+        np.save("test.npy", dataset)
+
+    def disconf_callback(self, disconf):
+        self.disconf = disconf.data
+
+    def audio_callback(self, audio):
+        if self.disconf == -1:
+            return
+        int_array = [ord(n) for n in audio.data]
+        # print int_array[0:10]
+        self.dataset.append((int_array, self.disconf))
+
+obj = MakeAudioDataset()
 rospy.spin()
